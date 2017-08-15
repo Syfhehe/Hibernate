185f62b99c1eab376318fc035140a99bb8397fa5
Syfhehe <Syfhehe@users.noreply.github.com>
2017-08-15 23:53:39 +0800

    Hibernate core API update

--- workspace/HibernateCoreAPI/test/com/syf/hibernate/model/xml/StudentTest.java
+++ workspace/HibernateCoreAPI/test/com/syf/hibernate/model/xml/StudentTest.java
@@ -44,7 +44,83 @@ public class StudentTest {
 		//Detached
 		stu.getAge();
 	}
+	
+	@Test
+	public void testGet() {
+		Session session = sf.getCurrentSession();
+		session.beginTransaction();
+		
+		Student stu = session.get(Student.class, 1);
+		session.getTransaction().commit();
+		System.out.println("name: "+ stu.getName());		
+	}
+	
+	@Test
+	public void testLoad() {
+		Session session = sf.getCurrentSession();
+		session.beginTransaction();
+		
+		Student stu = session.load(Student.class, 1);
+		session.getTransaction().commit();
+		//load生成的是代理对象 有id 但是name的属性为空
+		System.out.println("id: "+ stu.getId());	
+		//System.out.println("name: "+ stu.getName());		
+	}
 
+	//Detached变成Persistence状态
+	@Test
+	public void testUpdate1() {
+		Session session = sf.getCurrentSession();
+		session.beginTransaction();
+		
+		Student stu = session.get(Student.class, 1);
+		session.getTransaction().commit();
+		
+		stu.setName("ShenYifan");
+		
+		Session session2 = sf.getCurrentSession();
+		session2.beginTransaction();
+		session2.update(stu);
+		session2.getTransaction().commit();		
+	}
+	
+	//Transient一定要设置好ID 并且数据库里存在才可以 不然会报错
+	@Test
+	public void testUpdate2() {
+		Student stu = new Student();
+		stu.setId(1);
+		
+		stu.setName("ShenYifan");
+		
+		Session session2 = sf.getCurrentSession();
+		session2.beginTransaction();
+		session2.update(stu);
+		session2.getTransaction().commit();		
+	}
+	
+	//Persistence更新所有字段
+	@Test
+	public void testUpdate3() {
+		Session session = sf.getCurrentSession();
+		session.beginTransaction();
+		
+		Student stu = session.get(Student.class, 1);
+		stu.setName("ShenYifan");
+		session.getTransaction().commit();
+	}
+	
+	//Persistence更新想更新的字段 在XML中设置dynamic-update
+	@Test
+	public void testUpdate4() {
+		Session session = sf.getCurrentSession();
+		session.beginTransaction();
+		
+		Student stu = session.get(Student.class, 1);
+		stu.setName("ShenYifan1");
+		session.getTransaction().commit();
+	}
+	
+	
 	public static void main(String[] args) {
 		beforeClass();
 	}
