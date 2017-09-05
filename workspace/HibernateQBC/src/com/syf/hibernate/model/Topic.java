5475ce3880c4e462cbd7814896d40e95fde9982f
Syfhehe <Syfhehe@users.noreply.github.com>
2017-09-05 23:39:06 +0800

    QBC

---
+++ workspace/HibernateQBC/src/com/syf/hibernate/model/Topic.java
@@ -0,0 +1,69 @@
+package com.syf.hibernate.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Topic {
+	private int id;
+	private Date createDate;
+	private String title;
+	private Category category;
+	List<Msg> msgs = new ArrayList<Msg>();
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@OneToMany(mappedBy = "topic")
+	public List<Msg> getMsgs() {
+		return msgs;
+	}
+
+	public void setMsgs(List<Msg> msgs) {
+		this.msgs = msgs;
+	}
+
+	
+
+}
---
+++ workspace/HibernateQBC/test/com/syf/hibernate/model/OneToOneTest.java
@@ -0,0 +1,88 @@
+package com.syf.hibernate.model;
+
+import java.util.Date;
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Restrictions;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class OneToOneTest {
+
+	private static SessionFactory sf = null;
+
+	@BeforeClass
+	public static void beforeClass() {
+		Configuration cfg = new Configuration();
+		sf = cfg.configure().buildSessionFactory();
+	}
+
+	@AfterClass
+	public static void afterClass() {
+		sf.close();
+	}
+
+	@Test
+	public void test() {
+		Session session = sf.getCurrentSession();
+		session.beginTransaction();
+
+		for (int i = 0; i < 10; i++) {
+			Category c = new Category();
+			c.setName("c" + i);
+			session.save(c);
+		}
+
+		for (int i = 0; i < 10; i++) {
+			Category c = new Category();
+			c.setId(1);
+			Topic t = new Topic();
+			t.setCategory(c);
+			t.setTitle("t" + i);
+			t.setCreateDate(new Date());
+			session.save(t);
+		}
+
+		for (int i = 0; i < 10; i++) {
+			Topic t = new Topic();
+			t.setId(1);
+			Msg m = new Msg();
+			m.setCont("m" + i);
+			m.setTopic(t);
+			session.save(m);
+
+		}
+
+		session.getTransaction().commit();
+	}
+	
+	@Test
+	public void testQBC_1() {
+		Session session = sf.getCurrentSession();
+		session.beginTransaction();
+		
+		Criteria c = session.createCriteria(Topic.class)
+				.add(Restrictions.gt("id", 2))
+				.add(Restrictions.lt("id", 8))
+				.add(Restrictions.like("title", "t_"))
+				.createCriteria("category")
+				.add(Restrictions.between("id", 3, 5));
+		for(Object o: c.list()) {
+			Topic t = (Topic) o;
+			System.out.println(t.getId()+"-"+t.getTitle());
+		}
+		session.getTransaction().commit();
+	}
+	
+	
+	
+	
+
+}
